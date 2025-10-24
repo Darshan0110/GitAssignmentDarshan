bf3cea47975e3c5e6315f3a782f8a0c7eb65b4b9
Darshan Naidoo <Darshan.naidoo97@gmail.com>
2025-10-24 09:00:15 +0200

    Test 1: Rejects negative Numbers - GREEN

--- RomanNumerals/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumeralConverter.cs
@@ -2,9 +2,24 @@ namespace RomanNumerals
 {
     public class RomanNumeralConverter
     {
+        private const int MinimumValue = 1;
+        private const int MaximumValue = 3000;
+
         public string Convert(int number)
         {
+            if (IsOutsideValidRange(number))
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(number),
+                    $"Number must be between {MinimumValue} and {MaximumValue} inclusive.");
+            }
+
             return "";
+        }
+
+        private bool IsOutsideValidRange(int number)
+        {
+            return number < MinimumValue || number > MaximumValue;
         }
     }
 }
