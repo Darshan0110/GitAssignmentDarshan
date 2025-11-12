28f4fcf528614d88cfe7a5abdf713857ada10ad8
Darshan Naidoo <Darshan.naidoo97@gmail.com>
2025-11-12 10:30:15 +0200

    Test 7 Failed: Convert 2 to II - Refactored to lookup array

--- RomanNumerals/RomanNumeralsConverter.cs
+++ RomanNumerals/RomanNumeralsConverter.cs
@@ -4,6 +4,9 @@ namespace RomanNumerals
 {
     public class RomanNumeralConverter
     {
+
+        private static readonly string[] Ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        
         public string Convert(int number)
         {
             if (number < 1 || number > 3000)
@@ -13,10 +16,8 @@ namespace RomanNumerals
                     "Number must be between 1 and 3000 inclusive.");
             }
 
-            if (number == 1) return "I";
-            if (number == 5) return "V";
             
-            return "";
+            return Ones[number % 10];
         }
     }
 }
