d1ca34147f8024a70a7809649c85d45016309cba
Darshan Naidoo <Darshan.naidoo97@gmail.com>
2025-11-12 09:27:28 +0200

    Test 2: Failed Converter class does not exist

--- RomanNumerals.Test/RomanNumeralConverterTests.cs
+++ RomanNumerals.Test/RomanNumeralConverterTests.cs
@@ -1,10 +1,32 @@
-namespace RomanNumerals.Test;
+using Xunit;
+using RomanNumerals;
 
-public class UnitTest1
+namespace RomanNumerals.Test
 {
-    [Fact]
-    public void Test1()
+    public class RomanNumeralConverterTests
     {
+        private readonly RomanNumeralConverter _converter;
 
+        public RomanNumeralConverterTests()
+        {
+            _converter = new RomanNumeralConverter();
+        }
+
+        [Fact]
+        public void TestRunnerWorks()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void RejectsNegativeNumbers()
+        {
+            int negativeNumber = -1;
+
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(
+                () => _converter.Convert(negativeNumber));
+            
+            Assert.Contains("must be between 1 and 3000", exception.Message);
+        }
     }
 }
