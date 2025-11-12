8f2cec3ccb2a0404268a5ac6b2cf8dd5034036e1
Darshan Naidoo <Darshan.naidoo97@gmail.com>
2025-11-12 10:32:08 +0200

    Test 8 Failed: Convert 10 to x

--- RomanNumerals.Test/RomanNumeralConverterTests.cs
+++ RomanNumerals.Test/RomanNumeralConverterTests.cs
@@ -74,6 +74,14 @@ namespace RomanNumerals.Test
             Assert.Equal("II", result);
         }
 
+        [Fact]
+        public void Converts10ToX()
+        {
+            string result = _converter.Convert(10);
+            Assert.Equal("X", result);
+        }
+
+
         
     }
 }
