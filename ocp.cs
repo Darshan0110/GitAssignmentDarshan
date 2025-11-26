922962b8bf995b8dabcb06e2b8a164d6a86776ab
Darshan Naidoo <Darshan.naidoo97@gmail.com>
2025-11-26 10:09:26 +0200

    Refactor MembershipCostCalculator - removed if-else

--- ocp.cs
+++ ocp.cs
@@ -108,24 +108,9 @@ public class BasicMembership : Membership
     }
     public class MembershipCostCalculator
     {
-        public double CalculateMembership(Customer customer, int months)
+        public double CalculateMembershipCost(Customer customer, int months)
         {
-            if (customer.MembershipType == "Regular")
-            {
-                return 400 * months;
-            }
-            else if (customer.MembershipType == "Premium")
-            {
-                return months < 12 ? 600 * months : (500 * months) + 200;
-            }
-            else if (customer.MembershipType == "VIP")
-            {
-                return months < 18 ? 900 * months : (550 * months) + 300;
-            }
-            else
-            {
-                return 0.00; // What Now? Is this even right?
-            }
+            return customer.Membership.ClaculateMembershipCost(months);
         }
     }
 
