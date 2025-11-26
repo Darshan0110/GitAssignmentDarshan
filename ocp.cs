f2862e9ce920eeac5f9cb6ab4f1ebc909349c5e4
Darshan Naidoo <Darshan.naidoo97@gmail.com>
2025-11-26 09:57:36 +0200

    Refactor DiscountCalculator - removed if-else

--- ocp.cs
+++ ocp.cs
@@ -97,23 +97,11 @@ public class BasicMembership : Membership
 
     public class DiscountCalculator
     {
-        public double CalculateDiscount(Customer customer)
+        public double CalculateDiscount(Customer customer, double purchaseAmount)
         {
-            if (customer.MembershipType == "Regular")
-            {
-                return 0.05; // 5% discount
-            }
-            else if (customer.MembershipType == "Premium")
-            {
-                return 0.10; // 10% discount
-            }
-            else if (customer.MembershipType == "VIP")
-            {
-                return 0.20; // 20% discount
-            }
-            else
-            {
-                return 0.00; // No discount
+            
+                return customer.Membership.CalculateDiscount(purchaseAmount);
+
             }
         }
 
